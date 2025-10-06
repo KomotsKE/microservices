671500dc72ee53b9f93a5bb937d3a4c240966759
KomotsKE <alexkomov2005@gmail.com>
2025-10-06 21:31:55 +0500

    Add TokenController

---
+++ IdentityService/IdentityService.Api/Controllers/TokenController.cs
@@ -0,0 +1,47 @@
+using Azure.Core;
+using CoreLib.DTOs;
+using CoreLib.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TokenController : ControllerBase
+{
+    private readonly ITokenService _tokenService;
+
+    public TokenController(ITokenService tokenService)
+    {
+        _tokenService = tokenService;
+    }
+
+    /// <summary>
+    /// Обновление токенов по refresh-токену
+    /// </summary>
+    [HttpPost("refresh")]
+    [AllowAnonymous]
+    public async Task<IActionResult> RefreshTokens([FromBody] TokenRequest  request)
+    {
+        if (string.IsNullOrWhiteSpace(request.RefreshToken))
+            return BadRequest("Refresh token is required.");
+
+        var response = await _tokenService.RefreshTokensAsync(request.RefreshToken);
+        return Ok(response);
+    }
+
+    /// <summary>
+    /// Отзыв refresh-токена (выход из системы)
+    /// </summary>
+    [HttpPost("revoke")]
+    [Authorize]
+    public async Task<IActionResult> RevokeToken([FromBody] TokenRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.RefreshToken))
+            return BadRequest("Refresh token is required.");
+
+        await _tokenService.RevokeRefreshTokenAsync(request.RefreshToken);
+        return NoContent();
+    }
+}
