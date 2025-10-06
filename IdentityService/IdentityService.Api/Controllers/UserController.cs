2a28c5d7334241f3e0e59faa6a1f3a7fb21f8ddf
KomotsKE <alexkomov2005@gmail.com>
2025-10-06 21:29:44 +0500

    Add UserController

---
+++ IdentityService/IdentityService.Api/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+using CoreLib.DTOs;
+using CoreLib.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize(Roles = "ADMIN")] // Все ручки доступны только ADMIN
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    /// <summary>
+    /// Получить всех пользователей
+    /// </summary>
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _userService.GetAllUsersAsync();
+        return Ok(users);
+    }
+
+    /// <summary>
+    /// Получить пользователя по Id
+    /// </summary>
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetUserById(Guid id)
+    {
+        var user = await _userService.GetUserByIdAsync(id);
+        if (user == null)
+            return NotFound("User not found");
+
+        return Ok(user);
+    }
+
+    /// <summary>
+    /// Создать нового пользователя
+    /// </summary>
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserRequest request)
+    {
+        var user = await _userService.CreateUser(request);
+        return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+    }
+
+    /// <summary>
+    /// Обновить пользователя
+    /// </summary>
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateUser(Guid id, [FromBody] UserDto dto)
+    {
+        dto.Id = id;
+        await _userService.UpdateUserAsync(dto);
+        return NoContent();
+    }
+
+    /// <summary>
+    /// Удалить пользователя
+    /// </summary>
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteUser(Guid id)
+    {
+        await _userService.DeleteUserAsync(id);
+        return NoContent();
+    }
+}
